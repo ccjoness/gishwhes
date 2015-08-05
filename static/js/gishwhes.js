7f1b7383157324a76e4914384e2dbc340d0cddf9
Chris Jones <chris@pdxcodeguild.com>
2015-08-05 14:33:17 -0700

    fixed js error where wrong names were being passed to database

--- static/js/gishwhes.js
+++ static/js/gishwhes.js
@@ -1,12 +1,10 @@
 $(document).ready(function () {
-    // using jQuery
     function getCookie(name) {
         var cookieValue = null;
         if (document.cookie && document.cookie != '') {
             var cookies = document.cookie.split(';');
             for (var i = 0; i < cookies.length; i++) {
                 var cookie = jQuery.trim(cookies[i]);
-                // Does this cookie string begin with the name we want?
                 if (cookie.substring(0, name.length + 1) == (name + '=')) {
                     cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
                     break;
@@ -40,38 +38,4 @@ $(document).ready(function () {
     $('#gbutton').click(function () {
         return getName();
     });
-
-    var lc = LC.init(document.getElementsByClassName('literally')[0],
-        {imageURLPrefix: '/static/img'});
-    var name = $('#name0').html();
-    var first = $('#first').html();
-    var second = $('#second').html();
-    $('[data-action=upload-to-server]').click(function (e) {
-        e.preventDefault();
-
-        $('.submit').html('Uploading...');
-        // data argument;
-        $.ajax({
-            url: '/img_up/',
-            type: 'POST',
-            data: {
-                // convert the image data to base64
-                image: lc.canvasForExport().toDataURL().split(',')[1],
-                type: 'base64',
-                title: name,
-                first: first,
-                second: second
-            },
-            success: function (result) {
-                $('.submit').html('Uploaded!');
-                $.ajax({
-                    url: '/images/',
-                    success: function (data) {
-                        $('#images-refresh').html(data);
-                    }
-                });
-            }
-        });
-    });
-
 });
